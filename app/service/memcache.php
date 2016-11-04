e8fad94855a0c3a001fda27c0cfc4ce8c1936f8a
Oleg <oleg@aphixsoftware.com>
2016-11-04 00:17:41 +0000

    memcache fix

--- app/service/memcache.php
+++ app/service/memcache.php
@@ -1,14 +1,11 @@
 <?php
 
-function mc_init()
-{
-    global $memcache_pool, $config;
+function mc_init() {
+    global $memcached, $config;
 
-    $servers = $config['memcache'];
-    $firstServer = array_shift($servers);
-    $memcache_pool = memcache_connect($firstServer[0], $firstServer[1]);
-    foreach ($servers as $server) {
-        memcache_add_server($memcache_pool, $server[0], $server[1]);
+    $memcached = new Memcached();
+    foreach ($config['memcache'] as $server) {
+        $memcached->addServer($server[0], $server[1]);
     }
 }
 
@@ -18,28 +15,28 @@ function mc_init()
  * @param int $ttl
  * @return bool
  */
-function mc_set($key, $value, $ttl = 900)
-{
-    global $memcache_pool;
-    return memcache_set($memcache_pool, $key, $value, 0, $ttl);
+function mc_set($key, $value, $ttl = 900) {
+    /** @var Memcached $memcached */
+    global $memcached;
+    return $memcached->set($key, $value, $ttl);
 }
 
 /**
  * @param string|array $key
  * @return mixed
  */
-function mc_get($key)
-{
-    global $memcache_pool;
-    return memcache_get($memcache_pool, $key);
+function mc_get($key) {
+    /** @var Memcached $memcached */
+    global $memcached;
+    return $memcached->get($key);
 }
 
 /**
  * @param string $key
  * @return bool
  */
-function mc_delete($key)
-{
-    global $memcache_pool;
-    return memcache_delete($memcache_pool, $key);
+function mc_delete($key) {
+    /** @var Memcached $memcached */
+    global $memcached;
+    return $memcached->delete($key);
 }
